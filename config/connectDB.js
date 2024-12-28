8bee9f5296014de4a55a0f6336466ac126ee8053
Tushar Netale <tusharnetale007@gmail.com>
2024-12-29 00:58:54 +0530

    ready to deploy

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+require('dotenv').config()
 async function connectDB(){
     try {
         await mongoose.connect(process.env.MONGODB_URI)
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const connectDB = require('./config/connectDB')
 const router = require('./routes/index')
 const cookiesParser = require('cookie-parser')
 const { app, server } = require('./socket/index')
+require("dotenv").config
 
 // const app = express()
 app.use(cors({
